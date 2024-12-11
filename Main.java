48c7574f5a757fab1ea0c5f63dd92cef04d92954
Jarian Martinez <jarianm2@nycstudents.net>
2024-12-11 14:26:57 +0000

    wow

--- Main.java
+++ Main.java
@@ -1,34 +1,33 @@
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        int[] nums1 = {5, 8, 9, 10, 4};
-        int numStr1 = 5;
-        ArrayManipulator manipulator = new ArrayManipulator(nums1, numStr1);
-        System.out.println("------------");
-        System.out.println(manipulator.getNumsMiddleIndex());
-        System.out.println(manipulator.getNumsAverage());
-        System.out.println(manipulator.sameLengths());
-        System.out.println(manipulator.getNumAt(3));
-        System.out.println(manipulator.getWordAt(3));
-        System.out.println(manipulator.updateWordsListAt(3, "hello"));
-        System.out.println(manipulator.getWordAt(3));
-        System.out.println(manipulator.howLongLastWord());
-        System.out.println(manipulator.updateWordsListAt(4, "updated"));
-        System.out.println(manipulator.howLongLastWord());
+        
+        // Part A (Review)
+        // here is a string:
+        String myString = "JAVA IS FUN";
+
+        // use a for loop to print out each character
+        // of the string, one character per line, like this:
+        // J
+        // A
+        // V
+        // ...
+        // and so on:
+        
+        for(int i = 0; i < myString.length(); i++) {
+            System.out.println(myString.substring(i, i + 1));
+        }
         
         
         
-        
-        int[] nums2 = {2, 5, 7, 9};
-        int numStr2 = 6;
-        System.out.println("------------");
-        manipulator = new ArrayManipulator(nums2, numStr2);
-        System.out.println(manipulator.getNumsMiddleIndex());
-        System.out.println(manipulator.getNumsAverage());
-        System.out.println(manipulator.sameLengths());
-        System.out.println(manipulator.getNumAt(3));
-        System.out.println(manipulator.updateWordsListAt(6, "not valid"));
-        System.out.println(manipulator.howLongLastWord());
-        System.out.println(manipulator.updateWordsListAt(5, "valid"));
-        System.out.println(manipulator.howLongLastWord());
+        // PART B
+        // now here is an array of integers:
+        int[] nums = {15, 8, 10, 12, 7, 9, 4};
+
+        // figure out how to similarly use a for loop to print
+        // each element in the array, one number per line:
+
+        for(int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }  
     }
 }
